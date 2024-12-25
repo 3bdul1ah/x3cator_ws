401ffa607875cbcdaf8c4750533a0ef238b4275c
3bdul1ah <a2053231@gmail.com>
2024-12-25 19:39:52 +0800

    urdf

--- src/x3cator_hardware/motor_driver/motor_driver/x3cator_tf.py
+++ src/x3cator_hardware/motor_driver/motor_driver/x3cator_tf.py
@@ -23,65 +23,65 @@ class RobotTFPublisher(Node):
         base_trans.transform.rotation.z = q[2]
         base_trans.transform.rotation.w = q[3]
 
-        imu_trans = TransformStamped()
-        imu_trans.header.frame_id = "base_link"
-        imu_trans.child_frame_id = "imu_link"
-        imu_trans.transform.translation.x = 0.432
-        imu_trans.transform.translation.y = 0.125
-        imu_trans.transform.translation.z = 0.428
-        q = quaternion_from_euler(0, 0, 0)
-        imu_trans.transform.rotation.x = q[0]
-        imu_trans.transform.rotation.y = q[1]
-        imu_trans.transform.rotation.z = q[2]
-        imu_trans.transform.rotation.w = q[3]
+        # imu_trans = TransformStamped()
+        # imu_trans.header.frame_id = "base_link"
+        # imu_trans.child_frame_id = "imu_link"
+        # imu_trans.transform.translation.x = 0.432
+        # imu_trans.transform.translation.y = 0.125
+        # imu_trans.transform.translation.z = 0.428
+        # q = quaternion_from_euler(0, 0, 0)
+        # imu_trans.transform.rotation.x = q[0]
+        # imu_trans.transform.rotation.y = q[1]
+        # imu_trans.transform.rotation.z = q[2]
+        # imu_trans.transform.rotation.w = q[3]
 
-        lidar_trans = TransformStamped()
-        lidar_trans.header.frame_id = "base_link"
-        lidar_trans.child_frame_id = "lidar_link"  
-        lidar_trans.transform.translation.x = 0.437
-        lidar_trans.transform.translation.y = 0.0
-        lidar_trans.transform.translation.z = 0.429
-        q = quaternion_from_euler(0, 0, 0)
-        lidar_trans.transform.rotation.x = q[0]
-        lidar_trans.transform.rotation.y = q[1]
-        lidar_trans.transform.rotation.z = q[2]
-        lidar_trans.transform.rotation.w = q[3]
+        # lidar_trans = TransformStamped()
+        # lidar_trans.header.frame_id = "base_link"
+        # lidar_trans.child_frame_id = "lidar_link"  
+        # lidar_trans.transform.translation.x = 0.437
+        # lidar_trans.transform.translation.y = 0.0
+        # lidar_trans.transform.translation.z = 0.429
+        # q = quaternion_from_euler(0, 0, 0)
+        # lidar_trans.transform.rotation.x = q[0]
+        # lidar_trans.transform.rotation.y = q[1]
+        # lidar_trans.transform.rotation.z = q[2]
+        # lidar_trans.transform.rotation.w = q[3]
 
-        cam_trans = TransformStamped()
-        cam_trans.header.frame_id = "base_link"
-        cam_trans.child_frame_id = "camera_link"  
-        cam_trans.transform.translation.x = 0.53
-        cam_trans.transform.translation.y = 0.0
-        cam_trans.transform.translation.z = 0.13
-        q = quaternion_from_euler(0, 0, 0)
-        cam_trans.transform.rotation.x = q[0]
-        cam_trans.transform.rotation.y = q[1]
-        cam_trans.transform.rotation.z = q[2]
-        cam_trans.transform.rotation.w = q[3]
+        # cam_trans = TransformStamped()
+        # cam_trans.header.frame_id = "base_link"
+        # cam_trans.child_frame_id = "camera_link"  
+        # cam_trans.transform.translation.x = 0.53
+        # cam_trans.transform.translation.y = 0.0
+        # cam_trans.transform.translation.z = 0.13
+        # q = quaternion_from_euler(0, 0, 0)
+        # cam_trans.transform.rotation.x = q[0]
+        # cam_trans.transform.rotation.y = q[1]
+        # cam_trans.transform.rotation.z = q[2]
+        # cam_trans.transform.rotation.w = q[3]
 
-        left_wheel_trans = TransformStamped()
-        left_wheel_trans.header.frame_id = "base_link"
-        left_wheel_trans.child_frame_id = "left_wheel"
-        left_wheel_trans.transform.translation.x = 0.0
-        left_wheel_trans.transform.translation.y = 0.4
-        left_wheel_trans.transform.translation.z = -0.212
-        q = quaternion_from_euler(0, 0, 0)
-        left_wheel_trans.transform.rotation.x = q[0]
-        left_wheel_trans.transform.rotation.y = q[1]
-        left_wheel_trans.transform.rotation.z = q[2]
-        left_wheel_trans.transform.rotation.w = q[3]
+        # left_wheel_trans = TransformStamped()
+        # left_wheel_trans.header.frame_id = "base_link"
+        # left_wheel_trans.child_frame_id = "left_wheel"
+        # left_wheel_trans.transform.translation.x = 0.0
+        # left_wheel_trans.transform.translation.y = 0.4
+        # left_wheel_trans.transform.translation.z = -0.212
+        # q = quaternion_from_euler(0, 0, 0)
+        # left_wheel_trans.transform.rotation.x = q[0]
+        # left_wheel_trans.transform.rotation.y = q[1]
+        # left_wheel_trans.transform.rotation.z = q[2]
+        # left_wheel_trans.transform.rotation.w = q[3]
 
-        right_wheel_trans = TransformStamped()
-        right_wheel_trans.header.frame_id = "base_link"
-        right_wheel_trans.child_frame_id = "right_wheel"
-        right_wheel_trans.transform.translation.x = 0.0
-        right_wheel_trans.transform.translation.y = -0.4
-        right_wheel_trans.transform.translation.z = -0.212
-        q = quaternion_from_euler(0, 0, 0)
-        right_wheel_trans.transform.rotation.x = q[0]
-        right_wheel_trans.transform.rotation.y = q[1]
-        right_wheel_trans.transform.rotation.z = q[2]
-        right_wheel_trans.transform.rotation.w = q[3]        
+        # right_wheel_trans = TransformStamped()
+        # right_wheel_trans.header.frame_id = "base_link"
+        # right_wheel_trans.child_frame_id = "right_wheel"
+        # right_wheel_trans.transform.translation.x = 0.0
+        # right_wheel_trans.transform.translation.y = -0.4
+        # right_wheel_trans.transform.translation.z = -0.212
+        # q = quaternion_from_euler(0, 0, 0)
+        # right_wheel_trans.transform.rotation.x = q[0]
+        # right_wheel_trans.transform.rotation.y = q[1]
+        # right_wheel_trans.transform.rotation.z = q[2]
+        # right_wheel_trans.transform.rotation.w = q[3]        
 
         
         # rtk1_trans = TransformStamped()
@@ -121,7 +121,7 @@ class RobotTFPublisher(Node):
         # laser_trans.transform.rotation.w = q[3]
 
 
-        self.br.sendTransform([base_trans, imu_trans, lidar_trans, cam_trans, right_wheel_trans, left_wheel_trans])
+        self.br.sendTransform([base_trans])
 
 def main(args=None):
     rclpy.init(args=args)
