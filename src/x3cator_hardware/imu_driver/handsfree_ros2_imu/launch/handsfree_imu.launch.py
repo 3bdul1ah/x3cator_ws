e55cd0c7b1cd46e3a4753ae8f0d6d9c270a98079
3bdul1ah <a2053231@gmail.com>
2025-02-28 17:17:45 +0800

    nuc

--- src/x3cator_hardware/imu_driver/handsfree_ros2_imu/launch/handsfree_imu.launch.py
+++ src/x3cator_hardware/imu_driver/handsfree_ros2_imu/launch/handsfree_imu.launch.py
@@ -1,31 +1,17 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-
 
 def generate_launch_description():
-    imu_type_arg = DeclareLaunchArgument(
-        'imu_type',
-        default_value='a9',
-        description='IMU type [a9, b9, b6]'
-    )
-
-    imu_type = LaunchConfiguration('imu_type')
-
     imu_node = Node(
         package='handsfree_ros2_imu',
-        executable=[f'hfi_{imu_type.perform(None)}_ros2.py'],
+        executable='hfi_a9_ros2',  
         name='imu',
         output='screen',
         parameters=[{
             'port': '/dev/HFRobotIMU',
-            'gra_normalization': imu_type.perform(None) == 'a9'
+            'gra_normalization': True  
         }]
     )
 
-    return LaunchDescription([
-        imu_type_arg,
-        imu_node
-    ])
+    return LaunchDescription([imu_node])
